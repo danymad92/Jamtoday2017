9ab71fe52effbe05708bbff7f41bdad75175aa8f
JuanCarlosGdM <juancarlosgdm@gmail.com>
2017-10-21 19:34:26 +0200

    UI added

---
+++ Assets/Scripts/RecycleIcon.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RecycleIcon : MonoBehaviour {
+    public float speedRotation;
+
+    private RectTransform rectTrf;
+
+    void Awake() {
+        rectTrf = GetComponent<RectTransform>();
+    }
+
+    void OnEnable() {
+        //rectTrf.rotation = Quaternion.Euler(0, 0, 0);
+    }
+	
+	// Update is called once per frame
+	//void Update () {
+ //       rectTrf.rotation = Quaternion.Euler(0, 0, rectTrf.rotation.z + speedRotation * Time.deltaTime);
+	//}
+}
---
+++ Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreManager : MonoBehaviour {
+    public bool game;
+    public int pointsPerSecond;
+
+    private int score;
+	// Use this for initialization
+	void Start () {
+        game = true;
+        score = 0;
+        StartCoroutine(ScoreControl());
+    }
+
+	public IEnumerator ScoreControl () {
+        WaitForSeconds wfs = new WaitForSeconds(1/pointsPerSecond);
+		while (game) {
+            ++score;
+            MainCanvasController.SetScore(score);
+            yield return wfs;
+        }
+	}
+}
