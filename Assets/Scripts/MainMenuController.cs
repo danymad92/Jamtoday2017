94431b100eff58f6b47f44432e592802cdb81e8c
Francisco <paco@vzenix.es>
2017-10-22 12:33:36 +0200

    Menu principal

---
+++ Assets/Scripts/MainMenuController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuController : MonoBehaviour {
+
+	public void QuitGame() {
+		#if UNITY_EDITOR
+			UnityEditor.EditorApplication.isPlaying = false;
+		#else
+			Application.Quit();
+		#endif
+	}
+
+	public void GoToLevel(string level) {
+		Application.LoadLevel (level); 
+	}
+
+}
